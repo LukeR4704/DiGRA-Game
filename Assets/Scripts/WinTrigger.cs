3a1dfd850e0953a0729e9778dea4b2a8b9245e38
ViscBound <94448336+ViscBound@users.noreply.github.com>
2026-05-10 20:40:59 +0100

    cleaned up the game scene

--- Assets/Scripts/WinTrigger.cs
+++ Assets/Scripts/WinTrigger.cs
@@ -6,10 +6,12 @@ public class WinTrigger : MonoBehaviour
 {
     public GameObject winPanel;
     public GameObject purpleRain;
+    public GameObject player;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         winPanel.SetActive(true);
         purpleRain.SetActive(false);
+        player.SetActive(false);
     }
 }
