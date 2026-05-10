18ba3dba6c1dea6607aa6c9cf2df286c65ba88fe
ViscBound <94448336+ViscBound@users.noreply.github.com>
2026-05-10 18:54:58 +0100

    added scripts to tilemaps

--- Assets/Scripts/ColourChanger.cs
+++ Assets/Scripts/ColourChanger.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ColourChanger : MonoBehaviour
 {
-    private SpriteRenderer sr;
+    private Tilemap tile;
 
     public Color greenColor = Color.green;
     public Color purpleColor = Color.magenta;
 
     private void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
+        tile = GetComponent<Tilemap>();
         UpdateColor();
     }
 
@@ -21,9 +22,9 @@ public class ColourChanger : MonoBehaviour
     public void UpdateColor()
     {
         if (WorldStateManager.Instance.isPurpleWorld)
-            sr.color = purpleColor;
+            tile.color = purpleColor;
         else
-            sr.color = greenColor;
+            tile.color = greenColor;
     }
 
     private void OnEnable()
